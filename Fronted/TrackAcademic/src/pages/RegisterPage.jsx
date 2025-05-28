761f11cc5e591908b2554492cb5c4e3763334916
Raul Quigua <raul-quigua@hotmail.com>
2025-05-28 17:28:38 -0500

    hotfix: mateo canson

--- Fronted/TrackAcademic/src/pages/RegisterPage.jsx
+++ Fronted/TrackAcademic/src/pages/RegisterPage.jsx
@@ -2,23 +2,11 @@
 import Register from "../components/Register";
 
 export default function RegisterPage() {
-  const handleRegister = ({ firstName, lastName, email, password }) => {
-    console.log("Registro intentado con:", {
-      firstName,
-      lastName,
-      email,
-      password: "***" // No loggear la contraseña real por seguridad
-    });
-    
-    // Aquí puedes agregar la lógica para enviar los datos al servidor
-    // Por ejemplo:
-    // const userData = { firstName, lastName, email, password };
-    // registerUser(userData);
-  };
+
 
   return (
     <div>
-      <Register onRegister={handleRegister} />
+      <Register />
     </div>
   );
 }
