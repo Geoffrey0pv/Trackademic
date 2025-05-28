575740b3293a0762c08f2cc22be8292fee860e33
MateoRAR <mateorubio528@gmail.com>
2025-05-28 17:17:39 -0500

    feat: protect endpoints for user to be logged

--- Fronted/TrackAcademic/src/App.jsx
+++ Fronted/TrackAcademic/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage";
 import MainLayout from "./layout/Layout";
 import Dashboard from "./pages/Dashboard";
 import RegisterPage from "./pages/RegisterPage";
+import ProtectedLayout from "./layout/protectedLayout";
 
 
 
@@ -11,14 +12,20 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Dashboard />} />
-        </Route>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route element={<ProtectedLayout/>}>
+          <Route element={<MainLayout/>}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+/*
+
+*/
